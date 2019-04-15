3dc874122f6d0a3f96af04dc86c5b4e135ec378f
Lts <lts06069@naver.com>
2019-04-15 16:16:11 +0900

    other commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { range } = require('rxjs');
 const { map, filter } = require('rxjs/operators');
  
-range(1, 200).pipe(
+range(1, 100).pipe(
   filter(x => x % 2 === 1),
   map(x => x + x)
 ).subscribe(x => console.log(x));
